99ce7f8e62a355ffdfa7c867f4fcad78a4f9fa6e
PhanCuong86 <157564873+PhanCuong86@users.noreply.github.com>
2024-08-19 16:48:58 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -123,7 +123,7 @@ resource "aws_instance" "main_ec2" {
     ami = var.ami
     instance_type = var.instance-type
     vpc_security_group_ids = [ aws_security_group.security-group.id ]
-    key_name = aws_key_pair.cuong_key_rsa.id
+    key_name = aws_key_pair.cuong_key_rsa1.id
     user_data = "${file("install_nginx.sh")}"
     tags = {
         Name ="main-ec2" 
