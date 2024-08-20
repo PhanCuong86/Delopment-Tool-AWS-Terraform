abb635642a8f823620d4ae6f0011bed2ac4cf3a4
PhanCuong86 <157564873+PhanCuong86@users.noreply.github.com>
2024-08-20 17:45:35 +0700

    Update install_nginx.sh

--- install_nginx.sh
+++ install_nginx.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
-sudo apt-get update
-sudo apt-get install nginx -y
+sudo su
+pwd
+cd ..
+cd ..
+#ec2-user (Centos or Amazon Linux) 
+sudo yum update -y
+sudo yum install nginx -y
+sudo systemctl start nginx
+sudo systemctl enable nginx
 echo "<h1>Hello, DC1</h1>" | sudo tee /var/www/html/index.nginx-debian.html
-#sudo yum update -y 
-#sudo yum install -y nginx 
-#sudo service nginx start 
-#sudo chkconfig nginx on 
-#echo "<h1>Hello, DC1</h1>" | sudo tee /var/www/html/index.nginx-debian.html
+
