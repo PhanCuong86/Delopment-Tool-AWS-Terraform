fa7545b00a8431656d7bb4cb3639f9ad7f78d216
PhanCuong86 <157564873+PhanCuong86@users.noreply.github.com>
2024-08-20 16:10:30 +0700

    Update install_nginx.sh

--- install_nginx.sh
+++ install_nginx.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 sudo apt-get update
 sudo apt-get install nginx -y
-echo "<h1>Hi DC1-This is Github Action Pipeline</h1>" | sudo tee /var/www/html/index.nginx-debian.html
+echo "<h1>cuong phan</h1>" | sudo tee /var/www/html/index.nginx-debian.html
