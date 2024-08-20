c1c368da474c095c13c9ef5aa1f4343e2fc6c262
PhanCuong86 <157564873+PhanCuong86@users.noreply.github.com>
2024-08-20 16:13:03 +0700

    Update install_nginx.sh

--- install_nginx.sh
+++ install_nginx.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 sudo apt-get update
 sudo apt-get install nginx -y
-echo "<h1>cuong phan</h1>" | sudo tee /var/www/html/index.nginx-debian.html
+echo "<h1>Hello, DC1</h1>" | sudo tee /var/www/html/index.nginx-debian.html
